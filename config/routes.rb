9ff5ad2386a988ce7e6a02e079d9fdffb375f51f
Ashley Schauer <aelschauer@gmail.com>
2017-05-22 10:19:46 -0600

    add views for login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/login_signup'
+  root 'static_pages#root'
 
-  get 'static_pages/root'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    '/login',  to: 'sessions#new'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get    '/signup', to: 'users#new'
+  post   '/signup', to: 'users#create'
 end
