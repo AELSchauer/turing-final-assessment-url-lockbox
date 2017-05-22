7617619b572be403238e6b9b997f48e34f1fcf72
Ashley Schauer <aelschauer@gmail.com>
2017-05-22 10:17:40 -0600

    add sign up feature test

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -6,7 +6,9 @@ feature 'User can register for an account' do
 
     visit root_path
 
-    click_on 'Sign Up'
+    expect(current_path).to eq(login_path)
+
+    click_on 'Sign up here!'
 
     expect(current_path).to eq(register_path)
 
@@ -19,17 +21,63 @@ feature 'User can register for an account' do
     expect(page).to have_content("Welcome: #{user.email}")
   end
 
-  it 'but an error returns if an email is already taken' do
-    user = create(:user)
+  context 'errors' do
+    it 'but an error returns email is left blank' do
+      user = create(:user)
 
-    visit register_path
+      visit register_path
 
-    fill_in 'user[email]', with: user.email
-    fill_in 'user[password]', with: user.password
-    fill_in 'user[password_confirmation]', with: user.password
-    click_on 'Submit'
+      fill_in 'user[email]', with: ""
+      fill_in 'user[password]', with: user.password
+      fill_in 'user[password_confirmation]', with: user.password
+      click_on 'Submit'
 
-    expect(current_path).to eq(register_path)
-    expect(page).to have_content("Sorry, but that email has already been taken.")
+      expect(current_path).to eq(register_path)
+      binding.pry
+      expect(page).to have_content("Email can't be blank.")
+    end
+
+    it 'but an error returns if an email is already taken' do
+      user = create(:user)
+
+      visit register_path
+
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      fill_in 'user[password_confirmation]', with: user.password
+      click_on 'Submit'
+
+      expect(current_path).to eq(register_path)
+      expect(page).to have_content("Email already taken.")
+    end
+
+    it 'but an error returns if the password is blank' do
+      user = create(:user)
+
+      visit register_path
+
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      fill_in 'user[password_confirmation]', with: user.password
+      click_on 'Submit'
+
+      expect(current_path).to eq(register_path)
+      expect(page).to have_content("Password can't be blank.")
+      expect(page).to have_content("Password confirmation can't be blank.")
+    end
+
+    it 'but an error returns if the password does not match the confirmation' do
+      user = create(:user)
+
+      visit register_path
+
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      fill_in 'user[password_confirmation]', with: "bad confirmation"
+      click_on 'Submit'
+
+      expect(current_path).to eq(register_path)
+      expect(page).to have_content("Password confirmation doesn't match password.")
+    end
   end
 end
